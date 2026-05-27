e4213c646939154c3f1aecbf067452fe7fcc955f
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-05-27 18:34:41 +0530

    chatapp

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,10 +1,23 @@
 import dotenv from "dotenv";
 import { createServer } from "node:http";
+import { Server } from "socket.io";
 import app from "./app.js";
 dotenv.config();
 
 const port = process.env.PORT || 9090;
 const server = createServer(app);
+
+const io = new Server(server,{
+    cors : {
+        origin : "",
+        credentials : true,
+    }
+})
+
+
+
+
+
 
 const startDb = async () => {
   app.listen(port, () => {
