95077327a334469ed4ba90e8144f354aa76ec238
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-05-27 18:30:33 +0530

    chatapp

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,19 +1,16 @@
 import express from "express";
-import cors from "cors"
-import {cookieParser} from "cookie-parser";
-
-
-
-
-
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
 const app = express();
- 
-app.use(express.json())
-app.use(cors())
+
+app.use(express.json());
+app.use(cors());
 app.use(cookieParser());
 
+app.get("/", (req, res) => {
+  res.send("working");
+});
 
-app.get("/",(req,res)=>{
-    res.send("working")
-})
+
+export default app;
