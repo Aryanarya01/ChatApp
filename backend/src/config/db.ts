ce455cc5bf91067655d59790ca7ac369748333d9
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-05-27 18:44:38 +0530

    db

---
+++ backend/src/config/db.ts
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+const connectDb = async()=>{
+    try{
+        await mongoose.connect(process.env.MONGO_URI as string);
+        console.log("MongoDb connected")
+    }catch(err){
+        console.log(err);
+        process.exit(1)
+    }
+}
