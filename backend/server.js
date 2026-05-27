5a7f0f677025088115fb993527e951450fc8bc54
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-05-27 12:27:17 +0530

    server

--- backend/server.js
+++ backend/server.js
@@ -6,8 +6,9 @@ const client = new OpenAI({
 
 const response = await client.responses.create({
   model: 'gpt-5.2',
-  instructions: 'You are a coding assistant that talks like a pirate',
-  input: 'Are semicolons optional in JavaScript?',
+ 
+ 
+  input: 'Joke related to computer science',
 });
 
 console.log(response.output_text);
