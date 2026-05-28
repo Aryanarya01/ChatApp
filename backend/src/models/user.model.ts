760055e90b9fea567685487037b694ad8c2e3045
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-05-28 23:03:27 +0530

    initial

--- backend/src/models/user.model.ts
+++ backend/src/models/user.model.ts
@@ -1,6 +1,10 @@
 import mongoose from "mongoose";
 
-interface IUser extends Document
+interface IUser extends Document{
+name : String,
+username : String,
+email : st
+}
 
 const userSchema = new mongoose.Schema({
     name : {
