d8be6be18b6ba81f5edb6e73f717f4f282d9c137
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-05-29 22:24:52 +0530

    protect middleware

--- backend/src/middleware/protect.ts
+++ backend/src/middleware/protect.ts
@@ -1,6 +1,7 @@
-import type { Request, Response } from "express";
+import type { NextFunction, Request, Response } from "express";
 
 import jwt from "jsonwebtoken"
+import User from "../models/user.model.js";
 interface JwtPayload {
      id:string,
 }
@@ -8,15 +9,20 @@ export interface AuthRequest extends Request{
     user?: any,
 }
 
-const Protect = async(req:AuthRequest,res:Response)=>{
+const Protect = async(req:AuthRequest,res:Response,next:NextFunction)=>{
     try{
             const token = req.cookies.token;
             if(!token){
                 return res.status(401).json({message : "Not Authorized"})
             }
             const decoded = jwt.verify(token,process.env.JWT_SECRET as string) as JwtPayload;
-            const 
-            if()
+            const user = await User.findById(decoded.id).select("-password");
+            if(!user){
+                return res.status(401).json({message :"User not found"})
+            }
+            req.user = user;
+            next();
+            
     }catch(err){
         return res.status(401).json({message : "Invalid Token"})
     }
