38f25da00ba08c95dfaad6bacdc7a4b9126ea040
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-05-29 22:05:28 +0530

    generate token

--- backend/src/controllers/user.controller.ts
+++ backend/src/controllers/user.controller.ts
@@ -48,7 +48,29 @@ export const Login = async (req: Request, res: Response) => {
 
 export const login = async(req:Request,res:Response)=>{
     try{
-
+        const {email, password} = req.body;
+        if(!email||!password){
+            return res.status(400).json({message : "All fields are required!"});
+        }
+        const user = await User.findOne({email});
+        if(!user){
+            return res.status(404).json({message : "User not found!"})
+        }
+        const isMatched = await bcrypt.compare(password,user.password);
+        if(!isMatched){
+            return res.status(400).json({message : "Invalid credentials"})
+        }
+        const token = generateToken(user._id.toString());
+        return res.status(200).json({message : "Login Successful",
+            token,
+               user: {
+        _id: user._id,
+        name: user.name,
+        username: user.username,
+        email: user.email,
+        profilePicture: user.profilePicture,
+               },
+        })
     }catch(err){
         return res.status(500).json({message : "Server Error"});
     }
