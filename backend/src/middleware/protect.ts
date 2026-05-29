8e05d8fd9f522d7ac8eb86c8ed11cbebfd5d617d
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-05-29 22:22:22 +0530

    protect middleware

--- backend/src/middleware/protect.ts
+++ backend/src/middleware/protect.ts
@@ -1,14 +1,22 @@
 import type { Request, Response } from "express";
 
-
-
+import jwt from "jsonwebtoken"
+interface JwtPayload {
+     id:string,
+}
 export interface AuthRequest extends Request{
     user?: any,
 }
 
 const Protect = async(req:AuthRequest,res:Response)=>{
     try{
-        const 
+            const token = req.cookies.token;
+            if(!token){
+                return res.status(401).json({message : "Not Authorized"})
+            }
+            const decoded = jwt.verify(token,process.env.JWT_SECRET as string) as JwtPayload;
+            const 
+            if()
     }catch(err){
         return res.status(401).json({message : "Invalid Token"})
     }
