84eb9be82538240c4ba9f1f00a46445b375303a1
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-05-29 21:55:28 +0530

    generate token

--- backend/src/utils/generateToken.ts
+++ backend/src/utils/generateToken.ts
@@ -1,9 +1,9 @@
-import jwt from "jsonwebtoken"
+import jwt from "jsonwebtoken";
 
-const generateToken = (id:string)=>{
-    return jwt.sign({id},process.env.JWT_SECRET as string,{
-        expiresIn : "7d",
-    })
-}
+const generateToken = (id: string) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET as string, {
+    expiresIn: "7d",
+  });
+};
 
 export default generateToken;
