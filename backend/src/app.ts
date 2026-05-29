fd1d6f54970a3b8d9d24bb65b3a81977bc19ddde
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-05-29 21:47:09 +0530

    user model

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,13 +1,13 @@
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
-
+import userRoute from "./routes/user.route.js"
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 app.use(cookieParser());
-
+a
 app.get("/", (req, res) => {
   res.send("working");
 });
