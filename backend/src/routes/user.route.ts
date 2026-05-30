c09e6c986bc3da1c449a104c2380eae717944c4f
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-05-30 23:04:59 +0530

    login

--- backend/src/routes/user.route.ts
+++ backend/src/routes/user.route.ts
@@ -1,10 +1,10 @@
-import  { Router } from "express";
-import express from "express"
+import { Router } from "express";
+import express from "express";
 import { getMe, login, register } from "../controllers/user.controller.js";
 import Protect from "../middleware/protect.js";
 
 const router = express.Router();
 router.route("/register").post(register);
 router.route("/login").post(login);
-router.route("/me").get(Protect,getMe)
+router.route("/me").get(Protect, getMe);
 export default router;
