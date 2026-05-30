91b61941d80cbf0fb7138837e2b94fe108cbf5cc
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-05-30 23:36:29 +0530

    sockets

---
+++ backend/src/sockets/socketHandler.ts
@@ -0,0 +1,11 @@
+import type { Server, Socket } from "socket.io";
+
+const socketHandler = (io : Server)=>{
+    io.on("connection",(socket:Socket)=>{
+        console.log("user connected",socket.id);
+
+        socket.on("disconnect",()=>{
+            console.log("user disconnected",socket.id)
+        })
+    })
+}
