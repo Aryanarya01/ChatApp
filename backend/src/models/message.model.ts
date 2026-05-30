8c615f8f34a9c06be300f9f3f5dcf773b0e83fb0
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-05-30 23:57:35 +0530

    message  model

--- backend/src/models/message.model.ts
+++ backend/src/models/message.model.ts
@@ -1,11 +1,26 @@
 import type { Document } from "mongoose";
 import mongoose from "mongoose";
 
+
 interface IMessage extends Document{
     sender : mongoose.Schema.Types.ObjectId,
     conversation : mongoose.Schema.Types.ObjectId,
     content : mongoose.Schema.Types.ObjectId,
 }
 const messageSchema = new mongoose.Schema({
-
+    sender : {
+        type : mongoose.Schema.Types.ObjectId,
+        ref : "User",
+        required : true,
+    },
+    conversation : { 
+        type : mongoose.Schema.Types.ObjectId,
+        ref : "conversation",
+        required : true,
+    },
+    content : {
+        type : String,
+        required : true,
+        trim : true,
+    }
 })
