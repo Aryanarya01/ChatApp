084c8967fa0f1c0b5da9c91a93863db06c3e0cfe
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-05-30 23:14:31 +0530

    login

--- backend/src/controllers/user.controller.ts
+++ backend/src/controllers/user.controller.ts
@@ -96,3 +96,18 @@ export const getMe = async (req: AuthRequest, res: Response) => {
     return res.status(500).json({ message: "Server error" });
   }
 };
+
+
+export const logout = async(req:Request,res:Response)=>{
+    {
+        try{
+            res.cookie("token","",{
+                httpOnly : true,
+                expires : new Date(0)
+            });
+            res.status(200).json({message : "Logout successfully!"})
+        }catch(err){
+            return res.status(500).json({message : "Server error!"})
+        }
+    }
+}
