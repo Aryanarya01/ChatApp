a194448db8a62106b8e36ea2732a9f8ec648d851
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-05-30 23:49:49 +0530

    conversarion model

--- backend/src/models/conversation.model.ts
+++ backend/src/models/conversation.model.ts
@@ -1,21 +1,27 @@
 import type { Document } from "mongoose";
 import mongoose from "mongoose";
 
-interface IConversation extends Document{
-    participants : mongoose.Schema.Types.ObjectId[]
+interface IConversation extends Document {
+  participants: mongoose.Schema.Types.ObjectId[];
 }
 
-const consversationSchema = new mongoose.Schema({
-    participants : [
-        {
-            type : mongoose.Schema.Types.ObjectId,
-            ref : "User",
-            required : true,
-        }
-    ]
-},{
-    timestamps : true
-})
+const consversationSchema = new mongoose.Schema(
+  {
+    participants: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  },
+);
 
-const conversation = mongoose.model<IConversation>("conversation",consversationSchema);
+const conversation = mongoose.model<IConversation>(
+  "conversation",
+  consversationSchema,
+);
 export default conversation;
