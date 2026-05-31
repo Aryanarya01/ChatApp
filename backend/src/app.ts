798184897cfb7a2fcb6fcdd3ba45cfadc5dd56e4
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-05-31 23:07:54 +0530

    createconversation

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,12 +2,14 @@ import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import userRoute from "./routes/user.route.js"
+import conversationRoute from "./routes/conversation.route.js"
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 app.use(cookieParser());
-app.use(userRoute)
+app.use(userRoute);
+app.use(conversationRoute)
 app.get("/", (req, res) => {
   res.send("working");
 });
