e9850d9ef64bb0db0892d5d7210e77af2cadb6e1
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-05-31 23:06:06 +0530

    conversation route

---
+++ backend/src/routes/conversation.route.ts
@@ -0,0 +1,12 @@
+import { Router } from "express";
+import Protect from "../middleware/protect.js";
+import { createConversation, getConversation, getUserConversations } from "../controllers/conversation.controller.js";
+
+
+const router = Router();
+
+router.route("/create").post(Protect,createConversation);
+router.route("/").get(Protect,getUserConversations);
+router.route("/:id").get(Protect,getConversation)
+
+export default router;
