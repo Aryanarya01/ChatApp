17f43cd361de1263664122246f9e54e88c115b10
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-05-31 22:29:11 +0530

    createconversation

--- backend/src/controllers/conversation.controller.ts
+++ backend/src/controllers/conversation.controller.ts
@@ -1,11 +1,14 @@
 import type { Request, Response } from "express";
 import Conversation from "../models/conversation.model.js";
+import type { AuthRequest } from "../middleware/protect.js";
 
-export const createConversation = async(req:Request,res:Response)=>{
+export const createConversation = async(req:AuthRequest,res:Response)=>{
     try{
         const {recieverId} = req.body;
         const existingConversation = await Conversation.findOne({
-            p
+            participants : {
+                $all : [ req.user!._id,recieverId]
+            }
         })
         const conversation = await Conversation.create({
             participants : [
