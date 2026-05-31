3542a4b7b06b07b829f1a56b26830ab9d1e0a080
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-05-31 22:27:18 +0530

    createconversation

--- backend/src/controllers/conversation.controller.ts
+++ backend/src/controllers/conversation.controller.ts
@@ -3,7 +3,15 @@ import Conversation from "../models/conversation.model.js";
 
 export const createConversation = async(req:Request,res:Response)=>{
     try{
-        const conversation = await Conversation.crea
+        const {recieverId} = req.body;
+        const existingConversation = await Conversation.findOne({
+            p
+        })
+        const conversation = await Conversation.create({
+            participants : [
+
+            ]
+        })
     }catch(err){    
         return res.status(500).json({message : "Server Error"});
     }
