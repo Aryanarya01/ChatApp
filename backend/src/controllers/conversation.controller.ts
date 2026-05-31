0f99e6a69fa35767e7b1236d2c21863c1d6b363e
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-05-31 23:00:01 +0530

    createconversation

--- backend/src/controllers/conversation.controller.ts
+++ backend/src/controllers/conversation.controller.ts
@@ -38,3 +38,18 @@ export const getUserConversations = async(req:AuthRequest,res:Response)=>{
         })
     }
 }
+
+export const getConversation = async (req :AuthRequest, res : Response)=>{
+    try{
+        const {id} = req.params;
+        const conversation = await Conversation.findById(id).populate("participants","name username email profilePicture");
+        if(!conversation){
+            return res.status(404).json({message : "Conversation not found"})
+        }
+        return res.status(200).json(conversation)
+    }catch(err){
+        return res.status(500).json({
+            message : "Server error"
+        })
+    }
+}
