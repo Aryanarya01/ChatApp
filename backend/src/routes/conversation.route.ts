164b2036e380c09ddfff936069e4d60810277a93
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-05-31 23:06:18 +0530

    conversation route

--- backend/src/routes/conversation.route.ts
+++ backend/src/routes/conversation.route.ts
@@ -1,12 +1,15 @@
 import { Router } from "express";
 import Protect from "../middleware/protect.js";
-import { createConversation, getConversation, getUserConversations } from "../controllers/conversation.controller.js";
-
+import {
+  createConversation,
+  getConversation,
+  getUserConversations,
+} from "../controllers/conversation.controller.js";
 
 const router = Router();
 
-router.route("/create").post(Protect,createConversation);
-router.route("/").get(Protect,getUserConversations);
-router.route("/:id").get(Protect,getConversation)
+router.route("/create").post(Protect, createConversation);
+router.route("/").get(Protect, getUserConversations);
+router.route("/:id").get(Protect, getConversation);
 
 export default router;
