6b93a4c6960b0ee09fef8dfe1f1fa7d7c3f9f6e1
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-06-01 23:00:15 +0530

    message controller

---
+++ backend/src/controllers/message.controller.ts
@@ -0,0 +1,13 @@
+import type { Response } from "express";
+import type { AuthRequest } from "../middleware/protect.js";
+import Message from "../models/message.model.js";
+
+
+
+export const sendMessage = async(req:AuthRequest,res:Response)=>{
+    try{
+        const message = await Message
+    }catch(err){
+        return res.status(500).json({message : "Server Error"})
+    }
+}
