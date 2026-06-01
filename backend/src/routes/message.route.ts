da69908e59946ff1a196fe4b204c160ab8a3e4f8
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-06-01 23:35:44 +0530

    message route

--- backend/src/routes/message.route.ts
+++ backend/src/routes/message.route.ts
@@ -1,7 +1,11 @@
 import { Router } from "express";
+import Protect from "../middleware/protect.js";
+import { getMessage, sendMessage } from "../controllers/message.controller.js";
 
 const router = Router();
 
 
-router.route("/")
+router.route("/").post(Protect,sendMessage);
+router.route("/:conversationId").get(Protect,getMessage);
+
 export default router;
