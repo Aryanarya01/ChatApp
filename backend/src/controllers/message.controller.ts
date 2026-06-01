25c6075a57cce73365a4aa051afa36eb25d40078
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-06-01 23:14:17 +0530

    message controller

--- backend/src/controllers/message.controller.ts
+++ backend/src/controllers/message.controller.ts
@@ -1,12 +1,15 @@
 import type { Response } from "express";
 import type { AuthRequest } from "../middleware/protect.js";
 import Message from "../models/message.model.js";
+import Conversation from "../models/conversation.model.js";
 
 
 
 export const sendMessage = async(req:AuthRequest,res:Response)=>{
     try{
-        const message = await Message.create
+        const {conversationId, content} = req.body;
+        const conversation = await Conversation.findById(conversationId);
+        if
     }catch(err){
         return res.status(500).json({message : "Server Error"})
     }
