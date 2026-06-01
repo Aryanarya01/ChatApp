f8b710fcc3fce5cd071352a7f34a4c0ec0c3664c
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-06-01 23:18:39 +0530

    message controller

--- backend/src/controllers/message.controller.ts
+++ backend/src/controllers/message.controller.ts
@@ -3,22 +3,20 @@ import type { AuthRequest } from "../middleware/protect.js";
 import Message from "../models/message.model.js";
 import Conversation from "../models/conversation.model.js";
 
-
-
-export const sendMessage = async(req:AuthRequest,res:Response)=>{
-    try{
-        const {conversationId, content} = req.body;
-        const conversation = await Conversation.findById(conversationId);
-        if(!conversation){
-            return res.status(404).json({message : "Conversation not found"})
-        }
-        const message = await Message.create({
-            sender : req.user!._id,
-            conversation : conversationId,
-            content,
-        });
-        return res.status(201).json(message)
-    }catch(err){
-        return res.status(500).json({message : "Server Error"})
+export const sendMessage = async (req: AuthRequest, res: Response) => {
+  try {
+    const { conversationId, content } = req.body;
+    const conversation = await Conversation.findById(conversationId);
+    if (!conversation) {
+      return res.status(404).json({ message: "Conversation not found" });
     }
-}
+    const message = await Message.create({
+      sender: req.user!._id,
+      conversation: conversationId,
+      content,
+    });
+    return res.status(201).json(message);
+  } catch (err) {
+    return res.status(500).json({ message: "Server Error" });
+  }
+};
