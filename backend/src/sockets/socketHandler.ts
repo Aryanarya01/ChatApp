4766fe938998e844784d432f1481170f4afb3eb0
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-06-02 16:48:55 +0530

    message route

--- backend/src/sockets/socketHandler.ts
+++ backend/src/sockets/socketHandler.ts
@@ -1,8 +1,15 @@
 import type { Server, Socket } from "socket.io";
 
+const onlineUser = new Map<string,string>();
+
 const socketHandler = (io: Server) => {
   io.on("connection", (socket: Socket) => {
     console.log("user connected", socket.id);
+
+    socket.on("setup",(userId : string)=>{
+      onlineUser.set(userId,socket.id);
+      
+    })
 
     socket.on("disconnect", () => {
       console.log("user disconnected", socket.id);
