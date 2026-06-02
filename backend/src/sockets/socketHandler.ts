4ec7421652303db37701ed612c8dd8ae9e0b32f3
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-06-02 16:59:48 +0530

    socket

--- backend/src/sockets/socketHandler.ts
+++ backend/src/sockets/socketHandler.ts
@@ -1,28 +1,26 @@
 import type { Server, Socket } from "socket.io";
 
-const onlineUser = new Map<string,string>();
+const onlineUser = new Map<string, string>();
 
 const socketHandler = (io: Server) => {
   io.on("connection", (socket: Socket) => {
     console.log("user connected", socket.id);
 
-    socket.on("setup",(userId : string)=>{
-      onlineUser.set(userId,socket.id);
-      console.log("Online user :",onlineUser)
-    })
+    socket.on("setup", (userId: string) => {
+      onlineUser.set(userId, socket.id);
+      console.log("Online user :", onlineUser);
+    });
 
     socket.on("disconnect", () => {
-      for(const [userId, socketId] of onlineUser.entries()){
-         if(socketId === socket.id){
+      for (const [userId, socketId] of onlineUser.entries()) {
+        if (socketId === socket.id) {
           onlineUser.delete(userId);
           break;
-         }
+        }
       }
       console.log("user disconnected", socket.id);
     });
   });
 };
-
-
 
 export default socketHandler;
