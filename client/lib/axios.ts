70fd6faa4eaa1c900e3eaeb1f8f4973f0d7a91b8
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-06-02 17:20:20 +0530

    nextjs

---
+++ client/lib/axios.ts
@@ -0,0 +1,10 @@
+import axios from "axios"
+
+export const BASE_URL = "http://localhost:9090"
+
+const clientServer = axios.create({
+    baseURL : BASE_URL,
+    withCredentials : true
+})
+
+export default clientServer
