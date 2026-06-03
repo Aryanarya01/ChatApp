e34a4ed1c228995c6a311919eeefc653e237fca1
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-06-03 16:28:40 +0530

    chat

--- client/app/register/page.tsx
+++ client/app/register/page.tsx
@@ -1,40 +1,59 @@
-import clientServer from '@/lib/axios'
-import React, { useState } from 'react'
+import clientServer from "@/lib/axios";
+import React, { useState } from "react";
 
 const page = () => {
   const [formData, setFormData] = useState({
-    name : "",
-    username : "",
-    email : "",
-    password : ""
-  })
+    name: "",
+    username: "",
+    email: "",
+    password: "",
+  });
 
-  const handelChange = async(e:React.ChangeEvent<HTMLInputElement>)=>{
-      setFormData({
-        ...formData,
-        [e.target.name] : e.target.value
-      })
-  }
-  const handelSubmit = async(e:React.FormEvent)=>{
+  const handelChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+  const handelSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
-    try{
-      const res = await clientServer.post("/register",formData);
-      
-    }catch(err){
-      console.log(err)
+    try {
+      const res = await clientServer.post("/register", formData);
+    } catch (err) {
+      console.log(err);
     }
-  }
+  };
   return (
-    <div> 
+    <div>
       <form onSubmit={handelSubmit}>
-        <input type="text" placeholder='Enter your name' value={formData.name} onChange={handelChange} />
-        <input type="username" placeholder='Enter your username' value={formData.username} onChange={handelChange} />
-        <input type="email" placeholder='Enter your email' value={formData.email} onChange={handelChange} />
-        <input type="password" placeholder='Enter your password' value={formData.password} onChange={handelChange} />
-        <button type='submit'>Register</button>
+        <input
+          type="text"
+          placeholder="Enter your name"
+          value={formData.name}
+          onChange={handelChange}
+        />
+        <input
+          type="username"
+          placeholder="Enter your username"
+          value={formData.username}
+          onChange={handelChange}
+        />
+        <input
+          type="email"
+          placeholder="Enter your email"
+          value={formData.email}
+          onChange={handelChange}
+        />
+        <input
+          type="password"
+          placeholder="Enter your password"
+          value={formData.password}
+          onChange={handelChange}
+        />
+        <button type="submit">Register</button>
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default page;
