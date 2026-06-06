1ccc03e7d7e2d03fdbf314e67311c16afc286dea
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-06-06 22:22:33 +0530

    chat

--- client/app/chat/page.tsx
+++ client/app/chat/page.tsx
@@ -1,10 +1,11 @@
 
+import clientServer from '@/lib/axios'
 import React from 'react'
 
 const page = () => {
     const getMe = async()=>{
         try{
-
+            const data = await clientServer
         }catch(err){
             console.log(err)
         }
