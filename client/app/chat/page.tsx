dbc7bc191c39e79ee11583821a3b1b7c6643db73
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-06-06 22:20:24 +0530

    chat

--- client/app/chat/page.tsx
+++ client/app/chat/page.tsx
@@ -1,1 +1,20 @@
 
+import React from 'react'
+
+const page = () => {
+    const getMe = async()=>{
+        try{
+
+        }catch(err){
+            console.log(err)
+        }
+    }
+  return (
+    <div>
+        
+
+    </div>
+  )
+}
+
+export default page
