23ff5a2251ce531fc8c1c84f768e1d8b19ca0564
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-06-06 22:13:31 +0530

    chat

--- client/app/login/page.tsx
+++ client/app/login/page.tsx
@@ -3,29 +3,39 @@ import React, { ChangeEvent, useState } from "react";
 
 const page = () => {
   const [formData, setFormData] = useState({
-    email : "",
-    password : "",
-  })
-  const handelSubmit = async(e:React.FormEvent)=>{
-      e.preventDefault();
-      try{
-        const {data} = await clientServer.post("/register",formData);
-        console.log(data)
-      }catch(err){
-        console.log(err)
-      }
-  }
-  const handelChange = async(e:ChangeEvent<HTMLInputElement>)=>{
+    email: "",
+    password: "",
+  });
+  const handelSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    try {
+      const { data } = await clientServer.post("/register", formData);
+      console.log(data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  const handelChange = async (e: ChangeEvent<HTMLInputElement>) => {
     setFormData({
       ...formData,
-      [e.target.name] : e.target.value
-    })
-  }
+      [e.target.name]: e.target.value,
+    });
+  };
   return (
     <div>
       <form onSubmit={handelSubmit}>
-        <input type="email" placeholder="example : xyz@gmail.com" value={formData.email}onChange={handelChange}  />
-        <input type="password" placeholder="*********" value={formData.password} onChange={handelChange} />
+        <input
+          type="email"
+          placeholder="example : xyz@gmail.com"
+          value={formData.email}
+          onChange={handelChange}
+        />
+        <input
+          type="password"
+          placeholder="*********"
+          value={formData.password}
+          onChange={handelChange}
+        />
         <button type="submit">Login</button>
       </form>
     </div>
