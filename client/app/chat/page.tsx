80db247b5a3fa58f894a08a775daee86af6271b8
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-06-09 23:34:20 +0530

    chatPage

--- client/app/chat/page.tsx
+++ client/app/chat/page.tsx
@@ -6,7 +6,8 @@ const page = () => {
     const [users, setUsers] = useState([])
     const [selectedUser, setSelectedUser] = useState<any>(null);
     const [selectedConversation, setSelectedConversation] = useState<any>(null);
-    const [message, setMessage] = useState<any[]>([])
+    const [message, setMessage] = useState<any[]>([]);
+    const [content, setContent] = useState("")
   const getMe = async () => {
     try {
       const { data } = await clientServer.get("/auth/me");
