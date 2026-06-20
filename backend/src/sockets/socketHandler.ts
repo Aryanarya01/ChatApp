fbe76d6118ce583c0c7bfd71a144dd275ae2984c
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-06-20 22:56:40 +0530

    chat

--- backend/src/sockets/socketHandler.ts
+++ backend/src/sockets/socketHandler.ts
@@ -8,7 +8,7 @@ const socketHandler = (io: Server) => {
 
     socket.on("setup", (userId: string) => {
       onlineUser.set(userId, socket.id);
-      io.emit("onlineUsers")
+      io.emit("onlineUsers",[...onlineUser.keys()])
       console.log("Online user :", onlineUser);
     });
 
@@ -16,6 +16,7 @@ const socketHandler = (io: Server) => {
       for (const [userId, socketId] of onlineUser.entries()) {
         if (socketId === socket.id) {
           onlineUser.delete(userId);
+          io.emit("onlineUsers",[...onlineUser.keys()])
           break;
         }
       }
