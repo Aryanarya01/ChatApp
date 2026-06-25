9065af6b6c7812de596b0a552a44fa7abc2d1c42
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-06-25 14:06:43 +0530

    last message

--- backend/src/controllers/conversation.controller.ts
+++ backend/src/controllers/conversation.controller.ts
@@ -27,7 +27,10 @@ export const getUserConversations = async (req: AuthRequest, res: Response) => {
   try {
     const conversations = await Conversation.find({
       participants: req.user!._id,
-    }).populate("participants", "name username email profilePicture").populate("lastMessage").sort({updatedAt : -1});
+    })
+      .populate("participants", "name username email profilePicture")
+      .populate("lastMessage")
+      .sort({ updatedAt: -1 });
     return res.status(200).json(conversations);
   } catch (err) {
     return res.status(500).json({
