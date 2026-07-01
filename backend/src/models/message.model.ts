58fa2497fbdec07d2518709cb48f7d2d73d90a83
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-07-01 07:22:09 +0530

    seen part

--- backend/src/models/message.model.ts
+++ backend/src/models/message.model.ts
@@ -22,6 +22,10 @@ const messageSchema = new mongoose.Schema({
         type : String,
         required : true,
         trim : true,
+    },
+    seen : {
+        type : Boolean,
+        
     }
 },{
     timestamps : true,
