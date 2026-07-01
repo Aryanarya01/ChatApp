36125d89e6ed60341caacbf872b4dcbeb83aee72
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-07-01 07:43:18 +0530

    seen part

--- backend/src/routes/message.route.ts
+++ backend/src/routes/message.route.ts
@@ -1,11 +1,14 @@
 import { Router } from "express";
 import Protect from "../middleware/protect.js";
-import { getMessage, markAsSeen, sendMessage } from "../controllers/message.controller.js";
+import {
+  getMessage,
+  markAsSeen,
+  sendMessage,
+} from "../controllers/message.controller.js";
 
 const router = Router();
 
-
-router.route("/").post(Protect,sendMessage);
-router.route("/:conversationId").get(Protect,getMessage);
-router.route("/seen/:conversationId").patch(Protect,markAsSeen);
+router.route("/").post(Protect, sendMessage);
+router.route("/:conversationId").get(Protect, getMessage);
+router.route("/seen/:conversationId").patch(Protect, markAsSeen);
 export default router;
