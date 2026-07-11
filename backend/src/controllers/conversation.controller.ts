ca545564fc099db1ab299ee84e869f135c5039ed
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-07-11 20:14:35 +0530

    get conversations

--- backend/src/controllers/conversation.controller.ts
+++ backend/src/controllers/conversation.controller.ts
@@ -57,17 +57,4 @@ export const getConversation = async (req: AuthRequest, res: Response) => {
   }
 };
 
-
-
-export const getConversations = async(req : AuthRequest, res : Response)=>{
-  try{
-    const conversations = await Conversation.find({
-      participants : req.user!._id
-    }).populate("participants","name username profilePicture")
-    .populate("lastMessage")
-    .sort({updatedAt : -1});
-    res.status(200).json(conversations);
-  }catch(err){
-    return res.status(500).json({message : "Server Error"})
-  }
-}
+ 
