a11838db0edbacea985dd47077c41d9d40dcaab6
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-07-16 17:43:23 +0530

    chat Header

--- client/app/chat/page.tsx
+++ client/app/chat/page.tsx
@@ -152,35 +152,7 @@ const page = () => {
           {/* chatHeader */}
             <ChatHeader selectedUser={selectedUser} isTyping={isTyping}/>
 
-            <div className="flex-1 overflow-y-auto p-4">
-              <div className="flex flex-col gap-3 w-full">
-                {message.map((mess: any) => (
-                  <div
-                    key={mess._id}
-                    className={`max-w-[70%] p-3 rounded-lg ${
-                      mess.sender?._id === me?._id
-                        ? "self-end bg-blue-500 text-white"
-                        : "self-start bg-gray-200 text-black"
-                    }`}
-                  >
-                    <p>{mess.content}</p>
-                    <p className="text-xs mt-1">
-                      {new Date(mess.createdAt).toLocaleTimeString([], {
-                        hour: "2-digit",
-                        minute: "2-digit",
-                      })}
-                    </p>
-
-                    {mess.sender?._id === me?._id && (
-                      <p className="text-xs">
-                        {mess.seen ? "Seen" : "Delivered"}
-                      </p>
-                    )}
-                  </div>
-                ))}
-                <div ref={messageEndRef}></div>
-              </div>
-            </div>
+             
 
             <div className="border-t p-4 flex gap-2">
               <input
