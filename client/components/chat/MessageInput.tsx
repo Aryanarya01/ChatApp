a4faeb53d08d53ce1cc3916f1d365e00530eb6d4
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-07-16 18:01:56 +0530

    breaking components and asigining props

--- client/components/chat/MessageInput.tsx
+++ client/components/chat/MessageInput.tsx
@@ -1,8 +1,45 @@
 import React from 'react'
 
-const MessageInput = () => {
+interface MessageInputProps {
+    content : string,
+    setContent : React.Dispatch<React.SetStateAction<string>>,
+    selectedConversation : any,
+    typingTimeout : React.MutableRefObject<NodeJS.Timeout | null>,
+    handelSendMessage : ()=> void ,
+    socket : any,
+
+}
+
+const MessageInput = ({content,setContent,selectedConversation,typingTimeout,handelSendMessage,socket} : MessageInputProps) => {
   return (
-    <div>MessageInput</div>
+   <div className="border-t p-4 flex gap-2">
+              <input
+                className="flex-1 border rounded p-2"
+                placeholder="Type message..."
+                value={content}
+                onChange={(e) => {
+                  setContent(e.target.value);
+                  socket.emit("typing", selectedConversation._id);
+                  if (typingTimeout.current) {
+                    clearTimeout(typingTimeout.current);
+                  }
+                  typingTimeout.current = setTimeout(() => {
+                    socket.emit("stopTyping", selectedConversation._id);
+                  }, 1000);
+                }}
+                onKeyDown={(e) => {
+                  if (e.key === "Enter") {
+                    handelSendMessage();
+                  }
+                }}
+              />
+              <button
+                className="bg-blue-500 text-white px-4 rounded"
+                onClick={handelSendMessage}
+              >
+                Send
+              </button>
+            </div>
   )
 }
 
