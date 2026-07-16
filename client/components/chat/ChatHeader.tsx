54fc1c16658c3c820c6f517cf6bd35ace38160b1
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-07-16 17:40:04 +0530

    chat Header

---
+++ client/components/chat/ChatHeader.tsx
@@ -0,0 +1,22 @@
+interface chatHeader {
+    selectedUser : any,
+    isTyping : Boolean,
+}
+
+import React from 'react'
+
+const ChatHeader = ({selectedUser,isTyping} : chatHeader) => {
+  return (
+     
+     <div className="border-b p-4">   
+              
+              <h2 className="text-xl font-semibold">{selectedUser.name}</h2>
+              {isTyping && <p className="text-sm text-gray-500">Typing...</p>}
+            </div>
+    
+  )
+}
+
+export default ChatHeader
+
+ 
