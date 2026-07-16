1947963f0098c95085fe203e22a21a50aed5506f
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-07-16 17:46:10 +0530

    breaking components and asigining props

--- client/components/chat/MessageList.tsx
+++ client/components/chat/MessageList.tsx
@@ -1,8 +1,12 @@
 import React from 'react'
 
+interface MessageListProps {
+    message : any[],
+    me : any,
+    messageEndRef : React.RefObject<HTMLDivElement | null>
+}
 
-
-const MessageList = () => {
+const MessageList = ({message,me,messageEndRef} : MessageListProps) => {
   return (
     <div className="flex-1 overflow-y-auto p-4">
               <div className="flex flex-col gap-3 w-full">
