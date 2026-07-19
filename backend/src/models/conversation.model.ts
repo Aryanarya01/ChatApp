0e28339a7c87ce882d7a1025b82106cae8600d5d
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-07-19 18:53:15 +0530

    group messaging

--- backend/src/models/conversation.model.ts
+++ backend/src/models/conversation.model.ts
@@ -23,7 +23,14 @@ const consversationSchema = new mongoose.Schema(
       type : String,
       default : "",
     },
-    
+    groupImage : {
+      type : String,
+      default : "",
+    },
+    groupAdmin : {
+      type : mongoose.Schema.Types.ObjectId,
+      ref : "User",
+    },
     lastMessage: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "Message",
