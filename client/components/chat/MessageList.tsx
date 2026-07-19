23dd6c0124c4a62cb441acd81a31188797c7a602
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-07-19 18:49:16 +0530

    image showing functionality

--- client/components/chat/MessageList.tsx
+++ client/components/chat/MessageList.tsx
@@ -20,7 +20,7 @@ const MessageList = ({ message, me, messageEndRef }: MessageListProps) => {
             }`}
           >
            {mess.image && (
-            <img src={mess.image} alt="message" className="w-56 rounded-lg mb-2" />
+            <img src={`http://localhost:9090/${mess.image}`} alt="message" className="w-56 rounded-lg mb-2" />
            )}
            {mess.content && <p>{mess.content}</p>}
             <p className="text-xs mt-1">
