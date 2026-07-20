97b174604bfac65430a5d671f34adbb6983804f8
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-07-20 23:11:41 +0530

    creating group

--- client/components/chat/CreateGroupModal.tsx
+++ client/components/chat/CreateGroupModal.tsx
@@ -15,38 +15,52 @@ const CreateGroupModal = ({
   const [groupName, setGroupName] = useState("");
   const [selectedUsers, setSelectedUsers] = useState<string[]>([]);
 
-    const handleSelectUser = (userId : string)=>{
-      if(selectedUsers.includes(userId)){
-        setSelectedUsers(selectedUsers.filter((id)=>id !== userId))
-      }else{
-        setSelectedUsers([...selectedUsers, userId])
-      }
+  const handleSelectUser = (userId: string) => {
+    if (selectedUsers.includes(userId)) {
+      setSelectedUsers(selectedUsers.filter((id) => id !== userId));
+    } else {
+      setSelectedUsers([...selectedUsers, userId]);
     }
+  };
 
-    const handelCreateGroup = async()=>{
-      try{
-        const {data} = await clientServer.post("/conversation/group",{
-          groupName,
-          participants : selectedUsers,
-        });
-        console.log(data);
-        fetchConversations();
-        setOpenGroupModal(false)
-      }catch(err){
-        console.log(err)
-      }
+  const handelCreateGroup = async () => {
+    try {
+      const { data } = await clientServer.post("/conversation/group", {
+        groupName,
+        participants: selectedUsers,
+      });
+      console.log(data);
+      fetchConversations();
+      setOpenGroupModal(false);
+    } catch (err) {
+      console.log(err);
     }
+  };
 
-  return(
+  return (
     <div>
-        <input type="text" placeholder="Group Name" value={groupName} onChange={(e)=> setGroupName(e.target.value)} />
-        {users.map((user)=>(
-            <div key={user._id}>
-                <input type="checkbox" checked={selectedUsers.includes(user._id)} onChange={()=>handleSelectUser(user._id)}/>
-                <span>{user.name}</span>
-            </div>
-        ))}
-        <button onClick={()=>handelCreateGroup()} className="bg-blue-500 p-2 rounded m-3">Create Group</button>
+      <input
+        type="text"
+        placeholder="Group Name"
+        value={groupName}
+        onChange={(e) => setGroupName(e.target.value)}
+      />
+      {users.map((user) => (
+        <div key={user._id}>
+          <input
+            type="checkbox"
+            checked={selectedUsers.includes(user._id)}
+            onChange={() => handleSelectUser(user._id)}
+          />
+          <span>{user.name}</span>
+        </div>
+      ))}
+      <button
+        onClick={() => handelCreateGroup()}
+        className="bg-blue-500 p-2 rounded m-3"
+      >
+        Create Group
+      </button>
     </div>
   );
 };
