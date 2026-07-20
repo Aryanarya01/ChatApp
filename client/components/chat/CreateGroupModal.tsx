e9a1bae3f8a2304fcd06009edde72310844e56a8
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-07-20 16:21:24 +0530

    creategroupmodal

--- client/components/chat/CreateGroupModal.tsx
+++ client/components/chat/CreateGroupModal.tsx
@@ -1,10 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 interface groupModal {
-    
+    users : 
+    setOpenGroupModal :
+    fetchConversations :  
 }
 
 const CreateGroupModal = () => {
+    const [groupName, setGroupName] = useState("");
+    const [selectedUsers, setSelectedUsers] = useState<string[]>([]);
+
   return (
     <div>CreateGroupModal</div>
   )
