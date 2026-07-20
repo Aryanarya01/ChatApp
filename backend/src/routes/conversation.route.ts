e0961ad6a43ffe07cda9b1d94a3e050af944730d
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-07-20 22:54:54 +0530

    creategroupmodal

--- backend/src/routes/conversation.route.ts
+++ backend/src/routes/conversation.route.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import Protect from "../middleware/protect.js";
 import {
   createConversation,
+  createGroup,
   getConversation,
   getUserConversations,
 } from "../controllers/conversation.controller.js";
@@ -11,5 +12,6 @@ const router = Router();
 router.route("/create").post(Protect, createConversation);
 router.route("/").get(Protect, getUserConversations);
 router.route("/:id").get(Protect, getConversation);
+router.route("/group").post(Protect,createGroup);
 
 export default router;
