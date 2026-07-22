c1fd7a1a943206b4d403dd15aa9404bb33eaafd1
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-07-22 23:31:55 +0530

    group info modal props

--- client/components/chat/GroupInfoModal.tsx
+++ client/components/chat/GroupInfoModal.tsx
@@ -14,6 +14,7 @@ const GroupInfoModal = ({
   if (!open) {
     return null;
   }
+  const isAdmin = selectedConversation.groupAdmin._id === me._id;
   return (
     <div className="fixed inset-0 bg-black/40 flex items-center justify-center">
       <div className="bg-white p-5 rounded w-400px">
@@ -29,8 +30,12 @@ const GroupInfoModal = ({
         ))}
         <button onClick={() => setOpen(false)}>Close</button>
       </div>
-      <button>Add Member</button>
+      {isAdmin && (
+        <>
+         <button>Add Member</button>
      <button>Remove Member</button>
+        </>
+      )}
       <button>Leave Group</button>
     </div>
   );
