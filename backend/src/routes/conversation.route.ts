986c3312990737406724a569cd1b9b80d02c8d4f
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-07-22 11:38:15 +0530

    addmembertoGroup

--- backend/src/routes/conversation.route.ts
+++ backend/src/routes/conversation.route.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import Protect from "../middleware/protect.js";
 import {
+  addMemberToGroup,
   createConversation,
   createGroup,
   getConversation,
@@ -13,5 +14,6 @@ router.route("/create").post(Protect, createConversation);
 router.route("/").get(Protect, getUserConversations);
 router.route("/:id").get(Protect, getConversation);
 router.route("/group").post(Protect, createGroup);
-router.route("/addMember")
+router.route("/group/add-member").patch(Protect,addMemberToGroup);
+
 export default router;
