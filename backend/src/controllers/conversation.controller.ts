85b6492b894a1b7200a77a9be6216fff09d63c97
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-07-22 11:37:08 +0530

    addmembertoGroup

--- backend/src/controllers/conversation.controller.ts
+++ backend/src/controllers/conversation.controller.ts
@@ -105,26 +105,28 @@ export const createGroup = async (req: AuthRequest, res: Response) => {
   }
 };
 
-
-
-export const addMemberToGroup = async (req:AuthRequest, res : Response)=>{
-  try{
-      const {conversationId, userId} = req.body;
-      const group = await Conversation.findById(conversationId);
-      if(!group){
-        return res.status(404).json({message : "Group not found!"});
-      }
-      if(group.groupAdmin.toString() !== req.user!._id.toString()){
-        return res.status(403).json({message : "Only group admin can add members"});
-      }
-      const alreadyMember = group.participants.includes(userId);
-      if(alreadyMember){
-        return res.status(400).json({message : "User already in the group!"});
-      }
-      group.participants.push(userId);
-      await group.save();
-      const updatedGroup = await Conversation.findById(group._id).populate("participants","name username profilePicture").populate("groupAdmin","name username profilePicture")
-  }catch(err){
-      return res.status(500).json({message : "Server error!"})
+export const addMemberToGroup = async (req: AuthRequest, res: Response) => {
+  try {
+    const { conversationId, userId } = req.body;
+    const group = await Conversation.findById(conversationId);
+    if (!group) {
+      return res.status(404).json({ message: "Group not found!" });
+    }
+    if (group.groupAdmin.toString() !== req.user!._id.toString()) {
+      return res
+        .status(403)
+        .json({ message: "Only group admin can add members" });
+    }
+    const alreadyMember = group.participants.includes(userId);
+    if (alreadyMember) {
+      return res.status(400).json({ message: "User already in the group!" });
+    }
+    group.participants.push(userId);
+    await group.save();
+    const updatedGroup = await Conversation.findById(group._id)
+      .populate("participants", "name username profilePicture")
+      .populate("groupAdmin", "name username profilePicture");
+  } catch (err) {
+    return res.status(500).json({ message: "Server error!" });
   }
-}
+};
