2ffac60985bb5a029660d0d64762ceef8531c4b7
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-07-22 23:30:12 +0530

    group info modal props

--- client/components/chat/GroupInfoModal.tsx
+++ client/components/chat/GroupInfoModal.tsx
@@ -1,32 +1,39 @@
- import React, { use } from 'react'
- interface GroupInfoModalProp {
-    selectedConversation : any,
-    me : any,
-    open : boolean,
-    setOpen : React.Dispatch<React.SetStateAction<boolean>>,
+import React, { use } from "react";
+interface GroupInfoModalProp {
+  selectedConversation: any;
+  me: any;
+  open: boolean;
+  setOpen: React.Dispatch<React.SetStateAction<boolean>>;
 }
- const GroupInfoModal = ({selectedConversation,me,open,setOpen}:GroupInfoModalProp) => {
-    if(!open){
-        return null;
-    }
-   return (
-     <div className='fixed inset-0 bg-black/40 flex items-center justify-center'>
-        <div className="bg-white p-5 rounded w-400px">
-            <h2>{selectedConversation.groupName}</h2>
-            <h3>Members</h3>
-            {selectedConversation.participants.map((user:any)=>(
-                <div key={user._id}>
-                    {user.name}
-                    {selectedConversation.groupAdmin._id === user._id && (
-                        <span>(Admin)</span>
-                    )}
-                </div>
-            ))}
-            <button onClick={()=>setOpen(false)}>Close</button>
-        </div>
-        <button>Add Member</button> <button>Remove Member</button><button>Leave Group</button>
-     </div>
-   )
- }
- 
- export default GroupInfoModal
+const GroupInfoModal = ({
+  selectedConversation,
+  me,
+  open,
+  setOpen,
+}: GroupInfoModalProp) => {
+  if (!open) {
+    return null;
+  }
+  return (
+    <div className="fixed inset-0 bg-black/40 flex items-center justify-center">
+      <div className="bg-white p-5 rounded w-400px">
+        <h2>{selectedConversation.groupName}</h2>
+        <h3>Members</h3>
+        {selectedConversation.participants.map((user: any) => (
+          <div key={user._id}>
+            {user.name}
+            {selectedConversation.groupAdmin._id === user._id && (
+              <span>(Admin)</span>
+            )}
+          </div>
+        ))}
+        <button onClick={() => setOpen(false)}>Close</button>
+      </div>
+      <button>Add Member</button>
+     <button>Remove Member</button>
+      <button>Leave Group</button>
+    </div>
+  );
+};
+
+export default GroupInfoModal;
