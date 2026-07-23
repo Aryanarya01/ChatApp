bebd9c1ea4383b740f3b7c816ebd44aede362537
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-07-23 23:26:12 +0530

    groupinfomodal

--- client/components/chat/AddMemberModal.tsx
+++ client/components/chat/AddMemberModal.tsx
@@ -13,10 +13,20 @@ const AddMemberModal = ({open,setOpen,selectedConversation,users,fetchConversati
     const availableUsers = users.filter((user)=>{
       return !selectedConversation.participants.some((member : any)=> member._id === user._id)
     })
+    const handelAddMember = async(usersId : string)=>{
+      
+    }
   return (
     <div>
         <div className="bg-white p-5 rounded w-96">
             <h2 className="text-xl font-bold mb-4">Add Members</h2>
+            {
+              availableUsers.map((user : any)=>(
+                <div key={user._id}>
+                  <button onClick={()=>handelAddMember(user._id)}>{user.name}</button>
+                </div>
+              ))
+            }
             <button onClick={()=>setOpen(false)}>Close</button>
         </div>
     </div>
