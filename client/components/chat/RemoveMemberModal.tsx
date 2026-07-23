a533c356fab148a28ea35fe4c6db58ef08667265
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-07-23 23:43:49 +0530

    removemembermodal

--- client/components/chat/RemoveMemberModal.tsx
+++ client/components/chat/RemoveMemberModal.tsx
@@ -1,6 +1,12 @@
 
 
 import React from 'react'
+interface RemoveMemberModalProps {
+    open : boolean,
+    setOpen : React.Dispatch<React.SetStateAction<boolean>>;
+    selectedConversation : any,
+    fetchConversations : ()=> void,
+}
 
 const RemoveMemberModal = () => {
   return (
