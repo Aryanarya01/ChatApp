b99c37a811e3d3909ec8a69c4895ac4a9bc3471f
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-07-23 14:21:49 +0530

    addmembermodal

--- client/components/chat/AddMemberModal.tsx
+++ client/components/chat/AddMemberModal.tsx
@@ -1,7 +1,11 @@
 import React from 'react'
 
 interface AddMemberModalProps{
-    
+    open : boolean,
+    setOpen : React.Dispatch<React.SetStateAction<boolean>>,
+    selectedConversation : any,
+    users : any[],
+    fetchConversations : ()=> void,
 }
 
 const AddMemberModal = () => {
