a92c22f80cc1924a0a5746cdc2254c739514ca63
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-07-25 16:04:28 +0530

    rename group

--- backend/src/controllers/conversation.controller.ts
+++ backend/src/controllers/conversation.controller.ts
@@ -207,6 +207,7 @@ export const renameGroup = async (req: AuthRequest, res: Response) => {
     const updatedGroup = await Conversation.findById(group._id)
       .populate("participants", "name username profilePicture")
       .populate("groupAdmin", "name username profilePicture");
+      return res.status(200).json(updatedGroup);
   } catch (err) {
     return res.status(500).json({ message: "Server Error!" });
   }
