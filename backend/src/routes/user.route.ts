7627faa7d1fd704fac1332e192012d32192346ca
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-07-25 21:06:12 +0530

    updateProfilePicture

--- backend/src/routes/user.route.ts
+++ backend/src/routes/user.route.ts
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import express from "express";
-import { getAllUser, getMe, login, logout, register } from "../controllers/user.controller.js";
+import { getAllUser, getMe, login, logout, register, updateProfilePicture } from "../controllers/user.controller.js";
 import Protect from "../middleware/protect.js";
 
 const router = express.Router();
@@ -9,5 +9,6 @@ router.route("/login").post(login);
 router.route("/me").get(Protect, getMe);
 router.route("/logout").post(logout);
 router.route("/users").get(Protect,getAllUser);
+router.route("/profile-picture").patch(Protect, updateProfilePicture);
 
 export default router;
