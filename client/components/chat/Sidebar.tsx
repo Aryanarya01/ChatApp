ac4426df14b6cf07b6152fe4a90976d601b24828
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-07-28 15:54:34 +0530

    profile model

--- client/components/chat/Sidebar.tsx
+++ client/components/chat/Sidebar.tsx
@@ -28,18 +28,10 @@ const Sidebar = ({
   return (
     <>
       <div className="w-1/4 border-r p-4">
-        <button
-          className="w-full bg-blue-500 text-white p-2 rounded mb-3"
-          onClick={() => setOpenGroupModal(true)}
-        >
-          + New Group
-        </button>
-
-        <h2 className="text-xl font-bold mb-4">Chat page..</h2>
-
+      
         {/* Profile */}
         <div
-          className="border-t mt-4 pt-4 flex items-center gap-3 mb-4"
+          className="border-t mt-4 pt-4 flex items-center gap-3 mb-4 cursor-pointer hover:bg-gray-100 p-2 rounded"
           onClick={() => setOpenProfileModel(true)}
         >
           <img
@@ -52,6 +44,16 @@ const Sidebar = ({
             <p className="text-sm text-gray-500">{me?.email}</p>
           </div>
         </div>
+        <button
+          className="w-full bg-blue-500 text-white p-2 rounded mb-3"
+          onClick={() => setOpenGroupModal(true)}
+        >
+          + New Group
+        </button>
+
+        <h2 className="text-xl font-bold mb-4">Chat page..</h2>
+ 
+
         <hr />
         {/* conversation List */}
         {conversation.map((conv: any) => {
@@ -93,6 +95,7 @@ const Sidebar = ({
             </div>
           );
         })}
+        
       </div>
     </>
   );
