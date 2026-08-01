4ba60703cca6f7860f5cf0d009991565358050c4
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-08-01 14:53:05 +0530

    ui interface

--- client/components/chat/Sidebar.tsx
+++ client/components/chat/Sidebar.tsx
@@ -28,9 +28,8 @@ const Sidebar = ({
   return (
     <>
       <div className="w-[340px] bg-[#091827]/97 backdrop-blur-xl border-r border-white/10 flex flex-col">
-
         <div className="p-5 border-b border-white/10">
-        <h1 className="text-2xl font-bold text-white mb-5">🌊 ZenChat</h1>
+          <h1 className="text-2xl font-bold text-white mb-5">🌊 ZenChat</h1>
           <button
             className="w-full bg-blue-600 hover:bg-blue-700 text-white rounded-xl font-semibold py-3"
             onClick={() => setOpenGroupModal(true)}
@@ -38,8 +37,6 @@ const Sidebar = ({
             + New Group
           </button>
         </div>
-
-         
 
         {/* Profile */}
         <div
@@ -59,47 +56,64 @@ const Sidebar = ({
 
         <div className="border-b border-white/10 mx-4 mb-4"></div>
         <div className="flex-1 overflow-y-auto px-3 pb-4">
-        {/* conversation List */}
-        {conversation.map((conv: any) => {
-          const otherUser = conv.participants.find(
-            (p: any) => p._id !== me?._id,
-          );
-          return (
-            <div
-              key={conv._id}
-              className={`p-3 cursor-pointer ${selectedConversation?._id === conv._id ? "bg-gray-300" : "hover:bg-gray-100"}`}
-            >
+          {/* conversation List */}
+          {conversation.map((conv: any) => {
+            const otherUser = conv.participants.find(
+              (p: any) => p._id !== me?._id,
+            );
+            return (
               <div
-                onClick={() => {
-                  setSelectedConversation(conv);
-                  if (conv.isGroup) {
-                    setSelectedUser(null);
-                  } else {
-                    setSelectedUser(otherUser);
-                  }
-                }}
-                className="flex justify-between items-center"
+                key={conv._id}
+                className={`p-3 cursor-pointer mb-2 transition-all duration-600 ${selectedConversation?._id === conv._id ? "bg-blue-600 shadow-lg" : "hover:bg-white/10"}`}
               >
-                <img className="w-10 h-10 rounded-full object-cover" src={conv.isGroup ? conv.groupProfilePicture || "/group.png" :  otherUser?.profilePicture || "/avatar.png"} alt="" />
-                <h4>{conv.isGroup ? conv.groupName : otherUser?.name}</h4>
-                <div className="flex flex-row items-end gap-1">
-                  {!conv.isGroup && onlineUsers.includes(otherUser?._id) && (
-                    <span>🟢</span>
-                  )}
-                  {conv.unreadCount > 0 && (
-                    <span className="bg-green-500 text-white text-xs rounded-full px-2 py-1">
-                      {conv.unreadCount}
-                    </span>
-                  )}
-                </div>
-              </div>
+                <div
+                  onClick={() => {
+                    setSelectedConversation(conv);
+                    if (conv.isGroup) {
+                      setSelectedUser(null);
+                    } else {
+                      setSelectedUser(otherUser);
+                    }
+                  }}
+                  className="flex items-center justify-between"
+                >
+                  <div className="flex items-center gap-3">
+                  <img
+                    className="w-12 h-12 rounded-full object-cover"
+                    src={
+                      conv.isGroup
+                        ? conv.groupProfilePicture || "/group.png"
+                        : otherUser?.profilePicture || "/avatar.png"
+                    }
+                    alt=""
+                  />
+                
+                  <div>
+                       <h4 className="font-semibold text-white">{conv.isGroup ? conv.groupName : otherUser?.name}</h4>
+                       <div>
+                    {!conv.isGroup && onlineUsers.includes(otherUser?._id) && (
+                      <span className="w-2.5 h-2.5 rounded-full bg-green-500"></span>
+                    )}
+                    <span>{conv.isGroup ? "Group" : "Online"}</span>
+                    </div>
+                    </div>
+                    </div>
 
-              <p className="text-sm text-gray-500">
-                {conv.lastMessage?.content || "No messages"}
-              </p>
-            </div>
-          );
-        })}
+                    {/* right */}
+                    {conv.unreadCount > 0 && (
+                      <span className="min-w-6">
+                        {conv.unreadCount}
+                      </span>
+                    )}
+                  
+                </div>
+
+                <p className="text-sm text-slate-400 mt-2 truncate">
+                  {conv.lastMessage?.content || "No messages"}
+                </p>
+              </div>
+            );
+          })}
         </div>
       </div>
     </>
