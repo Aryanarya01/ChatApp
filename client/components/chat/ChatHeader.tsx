e26b894720b1cdddfb2659c0cd3f15b1842848ee
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-08-03 11:37:19 +0530

    interface

--- client/components/chat/ChatHeader.tsx
+++ client/components/chat/ChatHeader.tsx
@@ -4,35 +4,44 @@ interface chatHeaderProps {
   selectedUser: any;
   isTyping: Boolean;
   selectedConversation: any;
-  setOpenGroupInfo : React.Dispatch<React.SetStateAction<boolean>>
+  setOpenGroupInfo: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
 const ChatHeader = ({
   selectedUser,
   isTyping,
   selectedConversation,
-  setOpenGroupInfo
+  setOpenGroupInfo,
 }: chatHeaderProps) => {
   return (
     <div className="h-20 border-b border-white/10 bg-white/5 backdrop-blur-md px-6 flex items-center justify-between">
       {/* left */}
-    
-      <div className={`flex items-center gap-4 ${selectedConversation.isGroup ? "cursor-pointer" : ""}`} onClick={()=>{
-        if(selectedConversation?.isGroup){
-          setOpenGroupInfo(true)
-        }
-      }}>
-        <img src={selectedConversation?.isGroup 
-          ? selectedConversation.groupImage || "/group.png"
-          : selectedUser?.profilePicture || "/avatar.png"
-        } alt=""
+
+      <div
+        className={`flex items-center gap-4 ${selectedConversation.isGroup ? "cursor-pointer" : ""}`}
+        onClick={() => {
+          if (selectedConversation?.isGroup) {
+            setOpenGroupInfo(true);
+          }
+        }}
+      >
+        <img
+          src={
+            selectedConversation?.isGroup
+              ? selectedConversation.groupImage || "/group.png"
+              : selectedUser?.profilePicture || "/avatar.png"
+          }
+          alt=""
           className="w-12 h-12 rounded-full object-cover ring-2 ring-cyan-400"
-          />
+        />
         <div>
-        <h2> {selectedConversation?.isGroup
-          ? selectedConversation.groupName
-          : selectedUser.name}</h2>
-        </div>  
+          <h2>
+            {" "}
+            {selectedConversation?.isGroup
+              ? selectedConversation.groupName
+              : selectedUser.name}
+          </h2>
+        </div>
       </div>
       {isTyping && <p className="text-sm text-gray-500">Typing...</p>}
     </div>
