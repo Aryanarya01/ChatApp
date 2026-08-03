42ecf5c092302374d60d44f6163b969f1a08ea1c
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-08-03 13:01:59 +0530

    backend

--- client/components/chat/MessageList.tsx
+++ client/components/chat/MessageList.tsx
@@ -7,38 +7,52 @@ interface MessageListProps {
 }
 
 const MessageList = ({ message, me, messageEndRef }: MessageListProps) => {
-  return (
-    <div className="flex-1 overflow-y-auto px-8 py-6">
-      <div className="flex flex-col gap-5">
-        {message.map((mess: any) => (
-          <div
-            key={mess._id}
-            className={`max-w-[70%] p-3 rounded-lg ${
-              mess.sender?._id === me?._id
-                ? "self-end bg-blue-500 text-white"
-                : "self-start bg-gray-200 text-black"
-            }`}
-          >
-           {mess.image && (
-            <img src={`http://localhost:9090/${mess.image}`} alt="message" className="w-56 rounded-lg mb-2" />
-           )}
-           {mess.content && <p>{mess.content}</p>}
-            <p className="text-xs mt-1">
-              {new Date(mess.createdAt).toLocaleTimeString([], {
-                hour: "2-digit",
-                minute: "2-digit",
-              })}
-            </p>
+  // return (
+  //   <div className="flex-1 overflow-y-auto px-8 py-6">
+  //     <div className="flex flex-col gap-5">
+  //       {message.map((mess: any) => (
+  //         <div
+  //           key={mess._id}
+  //           className={`max-w-[70%] p-3 rounded-lg ${
+  //             mess.sender?._id === me?._id
+  //               ? "self-end bg-blue-500 text-white"
+  //               : "self-start bg-gray-200 text-black"
+  //           }`}
+  //         >
+  //          {mess.image && (
+  //           <img src={`http://localhost:9090/${mess.image}`} alt="message" className="w-56 rounded-lg mb-2" />
+  //          )}
+  //          {mess.content && <p>{mess.content}</p>}
+  //           <p className="text-xs mt-1">
+  //             {new Date(mess.createdAt).toLocaleTimeString([], {
+  //               hour: "2-digit",
+  //               minute: "2-digit",
+  //             })}
+  //           </p>
 
-            {mess.sender?._id === me?._id && (
-              <p className="text-xs">{mess.seen ? "Seen" : "Delivered"}</p>
-            )}
-          </div>
-        ))}
-        <div ref={messageEndRef}></div>
-      </div>
+  //           {mess.sender?._id === me?._id && (
+  //             <p className="text-xs">{mess.seen ? "Seen" : "Delivered"}</p>
+  //           )}
+  //         </div>
+  //       ))}
+  //       <div ref={messageEndRef}></div>
+  //     </div>
+  //   </div>
+  // );
+  return(
+    <div className="flex-1 overflow-y-auto px-8 py-6">
+  //     <div className="flex flex-col gap-5">
+    {message.map((mess :any)=>{
+      const isMe = mess.sender?._id === me?._id;
+      return(
+        <div key={mess._id} className={`flex ${isMe ? "justify-end" : "justify-start"}`}>
+          
+        </div>
+      )
+    })}
     </div>
-  );
+  </div>
+  )
 };
 
 export default MessageList;
