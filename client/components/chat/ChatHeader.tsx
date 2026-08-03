3af7fa484695c09dfb114b3144c5cc9d78160baf
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-08-03 11:54:29 +0530

    interface

--- client/components/chat/ChatHeader.tsx
+++ client/components/chat/ChatHeader.tsx
@@ -15,8 +15,10 @@ const ChatHeader = ({
 }: chatHeaderProps) => {
   return (
     <div className="h-20 border-b border-white/10 bg-white/5 backdrop-blur-md px-6 flex items-center justify-between">
-      {/* left */}
 
+
+      
+      {/* left */}
       <div
         className={`flex items-center gap-4 ${selectedConversation.isGroup ? "cursor-pointer" : ""}`}
         onClick={() => {
