2ff71b598d4b87946232b44f7bae9feacdeb4f49
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-08-05 11:53:19 +0530

    interface

--- client/components/chat/ProfileModal.tsx
+++ client/components/chat/ProfileModal.tsx
@@ -34,15 +34,23 @@ const ProfileModal = ({ me, open, setOpen,setMe }: ProfileModalProps) => {
 
   if (!open) return null;
   return (
-    <div className="fixed inset-0 bg-black/40 flex items-center justify-center">
-      <div className="bg-white p-5 rounded w-96">
-        <button onClick={()=>setOpen(false)}>✕</button>
+    <div className="fixed inset-0 z-50 bg-black/60 flex items-center justify-center backdrop-blur-sm">
+      <div className=" relative w-[430px] rounded-[30px] border border-cyan-400/20 bg-[#08131F]/90 backdrop-blur-2xl p-8 shadow-[0_0_50px_rgba(0,0,0,0.45)] ">
+        <button className="absolute top-5 right-5 text-slate-400 hover:text-white text-2xl transition" onClick={()=>setOpen(false)}>✕</button>
         <h2>My Profile</h2>
-        <img
-          src={me?.profilePicture || "/avatar.png"}
-          alt="Profile"
-          className="w-24 h-24 rounded-full object-cover mx-auto"
-        />
+        <p>Manage your Zen profile</p>
+{/* profile */}
+        <div>
+          <img
+            src={me?.profilePicture || "/avatar.png"}
+            alt="Profile"
+            className="w-24 h-24 rounded-full object-cover mx-auto"
+          />
+            <h3 className="text-center mt-3 font-semibold">{me?.name}</h3>
+        <p className="text-center text-gray-500">{me?.email}</p>
+        </div>
+        {/* upload */}
+        <div> 
         <input
           type="file"
           accept="image/*"
@@ -52,9 +60,11 @@ const ProfileModal = ({ me, open, setOpen,setMe }: ProfileModalProps) => {
             }
           }}
         />
-        <button className="bg-green-500" onClick={handleUploadImage}>Upload</button>
-        <h3 className="text-center mt-3 font-semibold">{me?.name}</h3>
-        <p className="text-center text-gray-500">{me?.email}</p>
+        <button className="bg-green-500" onClick={handleUploadImage}>Upload New Picture</button>
+       </div>
+       {/* divider */}
+       <div></div>
+       {/* close btn */}
         <button
           className="mt-5 w-full bg-red-500 text-white p-2 rounded"
           onClick={() => setOpen(false)}
