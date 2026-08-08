198845f7f411f4d79d2ef002a2cc621a7a8ea653
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-08-08 23:56:45 +0530

    interface

--- client/components/chat/ChatHeader.tsx
+++ client/components/chat/ChatHeader.tsx
@@ -17,7 +17,7 @@ const ChatHeader = ({
     <div className="h-20 border-b border-white/10 bg-white/5 backdrop-blur-md px-6 flex items-center justify-between">
       {/* left */}
       <div
-        className={`flex items-center gap-4 ${selectedConversation.isGroup ? "cursor-pointer" : ""}`}
+        className={`flex items-center gap-4 ${selectedConversation.isGroup || selectedConversation ? "cursor-pointer" : ""}`}
         onClick={() => {
           if (selectedConversation?.isGroup) {
             setOpenGroupInfo(true);
@@ -41,10 +41,10 @@ const ChatHeader = ({
           </h2>
 
           <div className="flex items-center gap-2 mt-1">
-            {selectedConversation?.isGroup && (
+            {selectedConversation?.isGroup || selectedConversation && (
               <>
                 <span className="w-2.5 h-2.5 rounded-full bg-emerald-400 shadow-[0_0_10px_#34d399]"></span>
-                <span className="text-sm text-slate-400">
+                <span className="text-sm text-slate-400 b">
                   {isTyping ? "Typing" : "Online"}
                 </span>
               </>
