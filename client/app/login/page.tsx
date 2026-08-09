e720a69fa9ee6bfb05aaba2ea5c4fcef173ff8a0
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-08-09 13:19:33 +0530

    register page

--- client/app/login/page.tsx
+++ client/app/login/page.tsx
@@ -26,21 +26,24 @@ const page = () => {
     }));
   };
   return (
-    <div>
-      <form onSubmit={handelSubmit}>
-        <input
-          type="email"
-          placeholder="example : xyz@gmail.com"
-          name="email"
-          onChange={handelChange}
-        />
-        <br />
-        <input
-          type="password"
-          placeholder="********"
-          name="password"
-          onChange={handelChange}
-        />
+    <div className="min-h-screen flex items-center justify-center bg-[#071320] relative overflow-hidden">
+      <div className="absolute w-96 h-96 bg-cyan-500/10 rounded-full blur-3xl -top-20 -left-20"></div>
+      <div className="absolute w-96 h-96 bg-sky-500/10 rounded-full blur-3xl -bottom-20 -right-20"></div>
+      <form className="relative z-10 w-[420px] rounded-[28px] border border-white/10 bg-white/5 backdrop-blur-2xl p-8 shadow-[0_0_40px_rgba(0,0,0,0.4)]" onSubmit={handelSubmit}>
+        <div>
+          <h1>ZenChat</h1>
+          <p>Login your account</p>
+        </div>
+        {/* email */}
+        <div>
+          <label>Email</label>
+          <input type="email" name="email" placeholder="Enter your email" onChange={handelChange}/>
+        </div>
+        {/* password */}
+        <div>
+          <label>Password</label>
+          <input type="password" name="password" placeholder="Enter your password" onChange={handelChange}/>
+        </div>
         <button type="submit">Login</button>
       </form>
     </div>
