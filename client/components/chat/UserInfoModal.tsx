7070d9a89e3b0ef5312e56158b5916228a97c960
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-08-09 11:59:16 +0530

    user info modal

--- client/components/chat/UserInfoModal.tsx
+++ client/components/chat/UserInfoModal.tsx
@@ -12,7 +12,7 @@ const UserInfoModal = ({open,setOpen,  selectedUser}:UserInfoModalProps) => {
   return (
     <>
         <div className='fixed inset-0 z-50 flex items-center justify-center bg-black/60 backdrop-blur-sm'>
-          <div>
+          <div className='w-[500px] rounded-[30px] border border-cyan-400/20  bg-[#08131F]/90 backdrop-blur-2xl p-8 shadow-[0_0_50px_rgba(0,0,0,0.45)]'>
             <div>
               <h2>User Info</h2>
               <button className='bg-green-600' onClick={()=>setOpen(false)}>✕</button>
