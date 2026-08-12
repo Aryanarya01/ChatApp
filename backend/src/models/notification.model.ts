6916c9b5ebc6538c37a92b708acbf4ec2a359825
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-08-12 12:42:59 +0530

    notification model

--- backend/src/models/notification.model.ts
+++ backend/src/models/notification.model.ts
@@ -1,48 +1,53 @@
 import mongoose, { Document } from "mongoose";
 
-interface INotification extends Document{
-    reciever : mongoose.Schema.Types.ObjectId,
-    sender : mongoose.Schema.Types.ObjectId,
-    conversation : mongoose.Schema.Types.ObjectId,
-    message : mongoose.Schema.Types.ObjectId,
-    type : "message",
-    read : boolean,
+interface INotification extends Document {
+  reciever: mongoose.Schema.Types.ObjectId;
+  sender: mongoose.Schema.Types.ObjectId;
+  conversation: mongoose.Schema.Types.ObjectId;
+  message: mongoose.Schema.Types.ObjectId;
+  type: "message";
+  read: boolean;
 }
 
-const notificationSchema = new mongoose.Schema({
-    reciever : {
-        type : mongoose.Schema.Types.ObjectId,
-        ref : "User",
-        required : true,
+const notificationSchema = new mongoose.Schema(
+  {
+    reciever: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
     },
-    sender : {
-        type : mongoose.Schema.Types.ObjectId,
-        ref : "User",
-        required : true,
+    sender: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
     },
-    conversation : {
-        type : mongoose.Schema.Types.ObjectId,
-        ref : "Conversation",
-        required : true,
+    conversation: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Conversation",
+      required: true,
     },
-    message : {
-        type : mongoose.Schema.Types.ObjectId,
-        ref : "Message",
-        required : true,
+    message: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Message",
+      required: true,
     },
-    type : {
-        type : String,
-        enum : ["message"],
-        default : "message",
+    type: {
+      type: String,
+      enum: ["message"],
+      default: "message",
     },
-    read : {
-        type : Boolean,
-        default : false,
+    read: {
+      type: Boolean,
+      default: false,
     },
-},{
-    timestamps : true,
-});
+  },
+  {
+    timestamps: true,
+  },
+);
 
-
-const Notification = mongoose.model<INotification>("Notification",notificationSchema);
+const Notification = mongoose.model<INotification>(
+  "Notification",
+  notificationSchema,
+);
 export default Notification;
