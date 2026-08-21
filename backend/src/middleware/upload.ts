cbd839a61221815ac9fb298c9f143e0f810f4071
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-08-21 13:57:54 +0530

    fixing the image issue

--- backend/src/middleware/upload.ts
+++ backend/src/middleware/upload.ts
@@ -1,14 +1,4 @@
 import multer from "multer";
+const storage = multer.memoryStorage();
 
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, "uploads/");
-  },
-  filename: (req, file, cb) => {
-    cb(null, `${Date.now()}-${file.originalname}`);
-  },
-});
-
-const upload = multer({ storage });
-
-export default upload;
+const upload = multer({storage})
